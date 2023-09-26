1a19ccec9330bdea066cf3c065af617b60c19268
dhiato1 <wertetenim@yahoo.com>
2023-09-26 20:22:41 +0100

    1.0.0.0.0.0

---
+++ Binaries/Intermediate/Build/Win64/UnrealEditor/Inc/Flippit/UHT/FlippitBPLibrary.generated.h
@@ -0,0 +1,106 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "FlippitBPLibrary.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FLIPPIT_FlippitBPLibrary_generated_h
+#error "FlippitBPLibrary.generated.h already included, missing '#pragma once' in FlippitBPLibrary.h"
+#endif
+#define FLIPPIT_FlippitBPLibrary_generated_h
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_SPARSE_DATA
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execOnAvatarRigLoaded);
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execOnAvatarRigLoaded);
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_ACCESSORS
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUFlippitBPLibrary(); \
+	friend struct Z_Construct_UClass_UFlippitBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UFlippitBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Flippit"), NO_API) \
+	DECLARE_SERIALIZER(UFlippitBPLibrary)
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_INCLASS \
+private: \
+	static void StaticRegisterNativesUFlippitBPLibrary(); \
+	friend struct Z_Construct_UClass_UFlippitBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UFlippitBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Flippit"), NO_API) \
+	DECLARE_SERIALIZER(UFlippitBPLibrary)
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFlippitBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFlippitBPLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFlippitBPLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFlippitBPLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFlippitBPLibrary(UFlippitBPLibrary&&); \
+	NO_API UFlippitBPLibrary(const UFlippitBPLibrary&); \
+public: \
+	NO_API virtual ~UFlippitBPLibrary();
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFlippitBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFlippitBPLibrary(UFlippitBPLibrary&&); \
+	NO_API UFlippitBPLibrary(const UFlippitBPLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFlippitBPLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFlippitBPLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFlippitBPLibrary) \
+	NO_API virtual ~UFlippitBPLibrary();
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_25_PROLOG
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_SPARSE_DATA \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_RPC_WRAPPERS \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_ACCESSORS \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_INCLASS \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_SPARSE_DATA \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_ACCESSORS \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_INCLASS_NO_PURE_DECLS \
+	FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h_28_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class FlippitBPLibrary."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FLIPPIT_API UClass* StaticClass<class UFlippitBPLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_dhia_Documents_Unreal_Projects_MyProject2_Plugins_Flippit_Source_Flippit_Public_FlippitBPLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Binaries/Source/Flippit/Public/FlippitCharacter.h
@@ -0,0 +1,171 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "WebSocketsModule.h" // Module definition
+#include "IWebSocket.h" 
+#include "Sound/SoundWaveProcedural.h"
+#include "Sound/AudioSettings.h"
+#include "Sound/AmbientSound.h"
+#include "Components/AudioComponent.h" // Include AudioComponent
+#include "FlippitCharacter.generated.h"
+
+UCLASS()
+class FLIPPIT_API AFlippitCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFlippitCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+public:
+	// Sets default values for this actor's properties
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString glbURL = FString();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString characterId = FString();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString ApiKey = FString();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString characterName = FString();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString Messagesrecieved = FString();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		int i ;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		float Delay;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		bool bMessageSentAndReceived = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		bool NotEmpy = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		bool Connected = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		bool Streaming = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		bool bAudioStreamingInProgress = false;
+	UPROPERTY(EditAnywhere , BlueprintReadWrite, Category = "Interaction Configuration")
+		bool Microphone= false;
+	UPROPERTY(EditAnywhere , BlueprintReadWrite, Category = "Interaction Configuration")
+		bool AvatarVoice= false;
+	UPROPERTY(EditAnywhere , BlueprintReadWrite, Category = "Interaction Configuration")
+		bool TextInteraction= false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString animationKeys;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString destinationkey;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString AWSSecretKey;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString AWSKey;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString OpenAIKey;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		FString audioByte;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction Configuration")
+		FKey Key;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "avatar")
+		TArray<USoundWave*> SoundWaveArray;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		class UBoxComponent* BoxCollisionComponent;
+
+
+
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+		TMap<FString, UAnimationAsset*> WordToAnimationMap;
+	// Array to store custom object information
+
+	TSharedPtr<IWebSocket> WebSocket;
+	bool bPlayerInInteractionZone;
+	bool bPlayerInCollisionZone;
+    bool notIn;  
+    bool audioishere;
+    bool hello;
+	int32 CurrentIndex;
+	FTimerHandle SoundFinishedHandle; // Declare the timer handle
+	TSet<USoundWave*> PlayedSounds; // Declare the set to track played sounds
+	FVector ActorLocation;
+	FString TemporaryAnimationKey;
+	AActor* LastVisitedActor = nullptr;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Custom Events")
+		void heytriggerme();
+	UFUNCTION(BlueprintCallable, Category = "WebSocket")
+		void EstablishWebSocketConnection();
+
+	// Function to send message over WebSocket
+
+	UFUNCTION(BlueprintCallable, Category = "WebSocket")
+		void SendWebSocketMessage(const FString& Message);
+
+	// Function to close WebSocket connection
+	UFUNCTION(BlueprintCallable, Category = "WebSocket")
+		void CloseWebSocketConnection();
+
+	// Function to handle player entering the interaction zone
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+		void OnPlayerEnterInteractionZone();
+
+	// Function to handle player exiting the interaction zone
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+		void OnPlayerExitInteractionZone();
+	// Function to handle player exiting the interaction zone
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+		FString HandleWebSocketMessage(const FString& Message);
+
+	UFUNCTION(BlueprintCallable, Category = "WebSocket")
+		void SendChatMessage(const FString& chatMessage);
+	UFUNCTION(BlueprintCallable, Category = "WebSocket")
+		void ConnectToWebSocket();
+	UFUNCTION(BlueprintCallable, Category = "WebSocket")
+		bool NotEmpty();
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		FString animationSetformresponse(FString animation);
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		void LogNamesOfAllObjectsInLevel();
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		FVector GetLocationOfActorByName(const FString& ActorNameToFind);
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		void DecodeAudioString(const FString& EncodedAudioString);
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		void PlaySoundsSequentially();
+
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		bool DeleteFile();
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		void OnEKeyPressed();
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		void OnEKeyReleased();
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		void SimulateForLoop(int32 FirstIndex, int32 LastIndex, float DelayBetweenLoops);
+
+	UFUNCTION()
+		void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// Function called when player exits the collision zone
+	UFUNCTION()
+		void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+		UAnimationAsset* AssignAnimationToString(FString Word1, UAnimationAsset* AnimationAsset1, FString Word2, UAnimationAsset* AnimationAsset2, FString Word3, UAnimationAsset* AnimationAsset3, FString Word4, UAnimationAsset* AnimationAsset4, FString Word5, UAnimationAsset* AnimationAsset5, FString Word6, UAnimationAsset* AnimationAsset6, FString Word7, UAnimationAsset* AnimationAsset7, FString Word8, UAnimationAsset* AnimationAsset8, FString Word9, UAnimationAsset* AnimationAsset9, FString Word10, UAnimationAsset* AnimationAsset10, FString Word11, UAnimationAsset* AnimationAsset11, FString Word12, UAnimationAsset* AnimationAsset12, FString Word13, UAnimationAsset* AnimationAsset13, FString ReceivedKey);
+
+
+};
+
+
+
+
